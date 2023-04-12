811a47e9ac1a1f11a9c46c94368128ee176f39af
ColeBradley-006 <85534478+ColeBradley-006@users.noreply.github.com>
2023-04-11 22:59:53 -0400

    DG Code Complete

--- Discontinuous Galerkin Code/graphing_code/main.py
+++ Discontinuous Galerkin Code/graphing_code/main.py
@@ -2,23 +2,68 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+filenames = ["0DGout.txt", "1DGout.txt", "2DGout.txt", "3DGout.txt"]
 
-file = np.loadtxt("DGout.txt")
+points = [10, 20, 40, 80]
+cur = []
+for each in filenames:
+    cur.append(np.loadtxt(each))
+file = np.loadtxt("DGSine5s.txt")
+
+exact1 = np.linspace(0, 2*np.pi, 10)
+exact1 = np.sin(exact1-0.5*5) + 2
+exact2 = np.linspace(0, 2*np.pi, 20)
+exact2 = np.sin(exact2-0.5*5) + 2
+exact3 = np.linspace(0, 2*np.pi, 40)
+exact3 = np.sin(exact3-0.5*5) + 2
+exact4 = np.linspace(0, 2*np.pi, 80)
+exact4 = np.sin(exact4-0.5*5) + 2
+
+y1 =  cur[0][8]
+y2 =  cur[1][8]
+y3 =  cur[2][8]
+y4 =  cur[3][8]
+x1 = np.linspace(0, 2*math.pi, 10)
+x2 = np.linspace(0, 2*math.pi, 20)
+x3 = np.linspace(0, 2*math.pi, 40)
+x4 = np.linspace(0, 2*math.pi, 80)
 
 
 
-
-exact = file[9]
-x = np.linspace(0, 40, 80)
-
-plt.plot(x, exact, label="exact")
-plt.xlabel("Grid Point")
+plt.plot(x1, y1, label="10 Elements")
+plt.plot(x2, y2, label="20 Elements")
+plt.plot(x3, y3, label="30 Elements")
+plt.plot(x4, y4, label="40 Elements")
+plt.plot(x4, exact4, label="Exact Solution")
+plt.xlabel("x")
 plt.ylabel("U(x,t)")
-plt.title("U at time t=5s for DG Scheme")
+plt.title("Advecting a Sine wave after 5s for DG Scheme")
 plt.legend()
-plt.savefig(str(points[i]) + "graph.png")
+plt.savefig("DGgraph.png")
 plt.show()
 
+y1error= exact1 - y1
+y2error= exact2 - y2
+y3error= exact3 - y3
+y4error= exact4 - y4
+
+S1 = np.sum(np.square(y1error))
+S2 = np.sum(np.square(y2error))
+S3 = np.sum(np.square(y3error))
+S4 = np.sum(np.square(y4error))
+
+errorPoints = np.array([math.log(math.sqrt(S1*2*math.pi/10)), math.log(math.sqrt(S2*2*math.pi/20)), math.log(math.sqrt(S3*2*math.pi/40)), math.log(math.sqrt(S4*2*math.pi/40))])
+errorX = np.array([math.log(10), math.log(20), math.log(40), math.log(80)])
+plt.plot(errorX, errorPoints, label="10 Elements")
+plt.xlabel("log(# of elements)")
+plt.ylabel("log(E)")
+plt.title("Error of DG scheme for varying # of elements")
+plt.legend()
+plt.savefig("errorGraph.png")
+plt.show()
+
+order = math.log((math.sqrt(S4*2*math.pi/40)-math.sqrt(S3*2*math.pi/40))/(math.sqrt(S3*2*math.pi/40)-math.sqrt(S2*2*math.pi/20)))/(math.log(2))
+print(order)
 """# This all graphs the error:
 
 errorData = np.loadtxt("errors.txt")
